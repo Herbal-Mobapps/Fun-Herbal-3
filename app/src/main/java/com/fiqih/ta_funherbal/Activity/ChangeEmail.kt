d83ab0e3bd3f11f41a39717817cb9499c7c8aed0
fiqih16 <fiqih1666@gmail.com>
2021-10-05 14:31:18 +0700

    Change Email & Password

---
+++ app/src/main/java/com/fiqih/ta_funherbal/Activity/ChangeEmail.kt
@@ -0,0 +1,12 @@
+package com.fiqih.ta_funherbal.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fiqih.ta_funherbal.R
+
+class ChangeEmail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_email)
+    }
+}
