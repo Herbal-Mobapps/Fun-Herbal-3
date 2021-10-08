120a5d7d1643489c1b99132031911a17eeec6343
fiqih16 <fiqih1666@gmail.com>
2021-10-08 16:00:04 +0700

    Image Slider

--- app/src/main/java/com/fiqih/ta_funherbal/Fragment/FindFragment.kt
+++ app/src/main/java/com/fiqih/ta_funherbal/Fragment/FindFragment.kt
@@ -7,32 +7,59 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.View.inflate
 import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.viewpager.widget.ViewPager
 import com.fiqih.ta_funherbal.Activity.KuisActivity
 import com.fiqih.ta_funherbal.Activity.LokasiActivity
+import com.fiqih.ta_funherbal.Adapter.AdapterSlider
+import com.fiqih.ta_funherbal.R
 import com.fiqih.ta_funherbal.VideoActivity.VideoActivity
 import com.fiqih.ta_funherbal.databinding.FragmentFindBinding
+import kotlinx.android.synthetic.main.fragment_find.*
 
 class FindFragment : Fragment() {
 
+    lateinit var vpSlider: ViewPager
+    private var _binding : FragmentFindBinding? = null
+    private val binding get() = _binding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        val bind = FragmentFindBinding.inflate(layoutInflater)
-        bind.btnVideo.setOnClickListener {
+        // image slider
+        val view: View = inflater.inflate(R.layout.fragment_find, container, false)
+
+        vpSlider = view.findViewById(R.id.vp_slider)
+
+        val arrSlider = ArrayList<Int>()
+        arrSlider.add(R.drawable.iconlogo)
+        arrSlider.add(R.drawable.icon_regis)
+        arrSlider.add(R.drawable.iconlogo)
+
+        val adapterSlider = AdapterSlider(arrSlider,activity)
+        vpSlider.adapter = adapterSlider
+        return view
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        _binding = FragmentFindBinding.bind(view)
+
+        binding?.btnVideo?.setOnClickListener {
             val intent = Intent(this@FindFragment.requireContext(), VideoActivity::class.java)
             startActivity(intent)
         }
-        bind.btnKuis.setOnClickListener {
+        binding?.btnKuis?.setOnClickListener {
             val intent = Intent(this@FindFragment.requireContext(),KuisActivity::class.java)
             startActivity(intent)
         }
-        bind.btnLokasi.setOnClickListener {
+        binding?.btnLokasi?.setOnClickListener {
             val intent = Intent(this@FindFragment.requireContext(),LokasiActivity::class.java)
             startActivity(intent)
         }
-        return bind.root
+
     }
 }
